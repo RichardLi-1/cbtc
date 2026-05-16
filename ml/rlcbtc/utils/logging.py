ad4a3b4d5dbd34b65b330a0b878cf5b938a75266
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2026-05-16 12:16:03 -0400

    ml(utils): stub warning/debug on training logger
    Matches SB3 callback logging surface until we wire structured logs.

--- ml/rlcbtc/utils/logging.py
+++ ml/rlcbtc/utils/logging.py
@@ -2,4 +2,11 @@ def get_logger(name: str):
     class _L:
         def info(self, msg):
             _ = (name, msg)
+
+        def warning(self, msg):
+            _ = (name, msg)
+
+        def debug(self, msg):
+            _ = (name, msg)
+
     return _L()
